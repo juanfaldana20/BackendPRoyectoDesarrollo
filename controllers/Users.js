a9687893323700303f1b1d5cc1da21fa817b2878
juanfaldana20 <98820412+juanfaldana20@users.noreply.github.com>
2023-05-25 16:29:18 -0500

    Add files via upload

---
+++ controllers/Users.js
@@ -0,0 +1,17 @@
+const listarUsuarios = async (req, res = express.request) => {
+    const Usuarios = await Usuarios.find().populate('tareas', 'title')
+    
+
+    try{
+        res.status({
+            ok:true,
+            usuarios,
+        })
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:' Error interno'
+        })
+    }
+}
